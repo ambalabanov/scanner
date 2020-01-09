b1548636875ac3b5eaec3f817f31c73dfc3269f0
ambalabanov <ambalabanov@gmail.com>
2020-01-09 16:01:01 +0500

    add dbInsert

--- main.go
+++ main.go
@@ -1,20 +1,48 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
 	"net"
 	"sync"
 )
 
-var wg sync.WaitGroup
+const (
+	uri        = "mongodb://localhost:27017"
+	Db         = "scannerDb"
+	Collection = "endpoints"
+)
+
+var (
+	wg sync.WaitGroup
+)
+
+type Endpoint struct {
+	Host string
+	Port int
+}
 
 func main() {
+	ports := []int{22, 80, 443, 8080}
 	host := "scanme.nmap.org"
-	for p := 1; p <= 1024; p++ {
+	for _, p := range ports {
 		wg.Add(1)
 		go scan(host, p)
 	}
 	wg.Wait()
+}
+
+func dbInsert(ep Endpoint) {
+	client := dbConnect()
+	collection := client.Database(Db).Collection(Collection)
+	insertResult, err := collection.InsertOne(context.TODO(), ep)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("dbInsert: ", insertResult.InsertedID, ep)
 }
 
 func scan(host string, port int) {
@@ -25,5 +53,22 @@ func scan(host string, port int) {
 		return
 	}
 	conn.Close()
-	fmt.Printf("%d open\n", port)
+	result := Endpoint{host, port}
+	dbInsert(result)
+}
+
+func dbConnect() *mongo.Client {
+	clientOptions := options.Client().ApplyURI(uri)
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(context.TODO(), nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	return client
 }
