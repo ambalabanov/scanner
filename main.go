69e3f4df730299154c4802586b0541306a80ec85
Andrey Balabanov <ambalabanov@gmail.com>
2020-01-10 20:50:32 +0500

    add http check

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"net/http"
+	"runtime"
 	"sync"
 
 	"go.mongodb.org/mongo-driver/bson"
@@ -29,64 +31,91 @@ type endpoint struct {
 }
 
 func main() {
-	collection, err := dbConnect()
-	if err != nil {
-		log.Fatalln("Db not connected!")
-	}
+
 	ports := []int{22, 80, 443, 8080}
 	hosts := []string{"scanme.nmap.org", "getinside.cloud"}
+	log.Println("Prepare db...")
 	for _, h := range hosts {
-		collection.DeleteMany(context.TODO(), bson.M{"host": h})
+		dbDelete(bson.M{"host": h})
 		for _, p := range ports {
-			go scan(collection, h, p)
+			go checkTCP(h, p)
 		}
 	}
+	log.Println("Start scan...")
+	log.Println("active gorutines", runtime.NumGoroutine())
 	wg.Wait()
-	filter := bson.M{}
-	results := dbFind(collection, filter)
-	for i := range results {
-		fmt.Println(results[i].Host, results[i].Port)
-	}
-
+	log.Println("Retrive data from db...")
+	dbFind(bson.M{})
+	log.Println("Done!")
 }
 
-func scan(collection *mongo.Collection, host string, port int) {
+func checkTCP(host string, port int) {
 	wg.Add(1)
 	defer wg.Done()
 	address := fmt.Sprintf("%s:%d", host, port)
 	conn, err := net.Dial("tcp", address)
+	if err == nil {
+		conn.Close()
+		dbInsert(bson.M{"host": host, "port": port})
+		go checkHTTP(host, port)
+	}
+
+}
+func checkHTTP(host string, port int) {
+	wg.Add(1)
+	defer wg.Done()
+	url := fmt.Sprintf("http://%s:%d", host, port)
+	response, err := http.Head(url)
 	if err != nil {
 		return
 	}
-	conn.Close()
-	result := endpoint{host, port}
-	dbInsert(collection, result)
+	if response.StatusCode == http.StatusOK {
+		dbInsert(bson.M{"host": host, "url": url})
+	}
+
 }
 
-func dbInsert(collection *mongo.Collection, ep endpoint) {
-
-	insertResult, err := collection.InsertOne(context.TODO(), ep)
+func dbInsert(data bson.M) {
+	collection, err := dbConnect()
+	if err != nil {
+		log.Fatalln("Db not connected!")
+	}
+	insertResult, err := collection.InsertOne(context.TODO(), data)
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Println("dbInsert: ", insertResult.InsertedID, ep)
+	log.Println("dbInsert: ", insertResult.InsertedID, data)
 }
+func dbDelete(filter bson.M) {
+	collection, err := dbConnect()
+	if err != nil {
+		log.Fatalln("Db not connected!")
+	}
+	deleteResult, err := collection.DeleteMany(context.TODO(), filter)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("dbDelete ", deleteResult.DeletedCount, filter)
+}
+func dbFind(filter bson.M) {
+	collection, err := dbConnect()
+	if err != nil {
+		log.Fatalln("Db not connected!")
+	}
 
-func dbFind(collection *mongo.Collection, filter bson.M) []*endpoint {
-	var results []*endpoint
 	cur, err := collection.Find(context.TODO(), filter)
 	if err != nil {
 		log.Fatal("Error on Finding all the documents", err)
 	}
 	for cur.Next(context.TODO()) {
-		var result endpoint
+		var result bson.M
 		err = cur.Decode(&result)
 		if err != nil {
 			log.Fatal("Error on Decoding the document", err)
 		}
-		results = append(results, &result)
+		fmt.Println(result)
 	}
-	return results
+
 }
 
 func dbConnect() (*mongo.Collection, error) {
